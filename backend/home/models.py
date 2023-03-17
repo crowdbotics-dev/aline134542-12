ba22fc98e119b0f47ea3d8d395e8e3b8b642ba80
crowdbotics-dev <engineering@crowdbotics.com>
2023-03-17 18:50:24 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,9 @@
+from django.conf import settings
 from django.db import models
+class Country(models.Model):
+    'Generated Model'
+    name = models.CharField(max_length=100,)
+    abbrev = models.CharField(max_length=3,)
+    population = models.IntegerField()
+    area = models.IntegerField()
+    gdp = models.FloatField()
